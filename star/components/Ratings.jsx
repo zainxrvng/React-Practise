ace24eb18a6fd3480f1b093cfb458b0e2711a127
zainxrvng <muhammadzainkhan29@gmail.com>
2026-07-08 18:33:28 +0500

    Implement rating component with star click and hover functionality for practise may change it later

--- star/components/Ratings.jsx
+++ star/components/Ratings.jsx
@@ -1,8 +1,22 @@
 const Ratings = () => {
+  const stars = Array.from({ length: 5 }, (_, i) => i + 1);
+  const clicked = () => console.log("clicked")
+  const hovered = (direction) => console.log("hovered",direction)
   return (
     <>
       <div className="rating-container">
         <h2>rate your expreicance</h2>
+        <div className="stars">
+          {stars.map((stars) => (
+            <span onClick={clicked}
+            onMouseEnter={() => {hovered("enter")}}
+            onMouseLeave={() => {hovered("leave")}}
+             className="star" key={stars}>
+              {" "}
+              {"\u2605"}{" "}
+            </span>
+          ))}
+        </div>
       </div>
     </>
   );
--- star/src/App.jsx
+++ star/src/App.jsx
@@ -1,16 +1,14 @@
-import './App.css'
-import Ratings from '../components/Ratings'
+import "./App.css";
+import Ratings from "../components/Ratings";
 
 function App() {
-  const name = "zain"
   return (
     <>
       <div>
-        <h1>hello {name}</h1>
         <Ratings />
       </div>
     </>
   );
 }
 
-export default App
+export default App;
