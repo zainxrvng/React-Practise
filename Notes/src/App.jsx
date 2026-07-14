ac65beac4ec028b9365a22031aaa9fa9c7c1c0e3
zainxrvng <muhammadzainkhan29@gmail.com>
2026-07-14 19:34:57 +0500

    created Notes app

---
+++ Notes/src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="m-w-lg mx-auto mt-10 p-6 bg-gray-100 rounded-lg shadow-lg">
+      <h2 className="text-2xl font-bold mb-4 text-center">Notes app</h2>
+    </div>
+  )
+}
+
+export default App
