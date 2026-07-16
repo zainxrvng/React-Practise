3d8667d3a5a90b5d1cba6a0d3cc7e69015b6d59e
zainxrvng <muhammadzainkhan29@gmail.com>
2026-07-16 22:50:59 +0500

    Add Newforms component for note creation with title, priority, category, and description fields

--- Notes/src/App.jsx
+++ Notes/src/App.jsx
@@ -1,9 +1,11 @@
-import Forms from "./components/Forms"
+// import Forms from "./components/Forms"
+import Newforms from "./components/Newform"
 const App = () => {
   return (
     <div className="m-w-lg mx-auto mt-10 p-6 bg-gray-100 rounded-lg shadow-lg">
       <h2 className="text-2xl font-bold mb-4 text-center">Notes app</h2>
-      <Forms />
+      {/* <Forms /> */}
+      <Newforms />
     </div>
   )
 }
---
+++ Notes/src/components/Newform.jsx
@@ -0,0 +1,81 @@
+import { useState } from "react";
+const Newforms = () => {
+  const [formData, setFormData] = useState({
+    Title: "",
+    Priorty: "Medium",
+    Category: "Work",
+    Description: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData({...formData, [e.target.name]: e.target.value})
+  }
+  console.log(formData);
+  return (
+    <div>
+      <form className="mb-6">
+        <div className="mb-4">
+          <label htmlFor="title" className="block font-semibold">
+            Title
+          </label>
+          <input
+            name="Title"
+            type="text"
+            value={formData.Title}
+            className="w-full rounded p-2 border"
+            onChange={handleChange}
+          />
+          <div className="mb-4">
+            <label htmlFor="priority" className="block font-semibold">
+              priority
+            </label>
+            <select
+              name="Priorty"
+              type="text"
+              value={formData.Priorty}
+              className="w-full rounded p-2 border"
+              onChange={handleChange}
+            >
+              <option value="High">🔴High</option>
+              <option value="Medium">🟠 Medium</option>
+              <option value="Low">🟢 Low</option>
+            </select>
+          </div>
+          <div className="mb-4">
+            <label htmlFor="Category" className="block font-semibold">
+              Category
+            </label>
+            <select
+              name="Category"
+              type="text"
+              value={formData.Category}
+              className="w-full rounded p-2 border"
+              onChange={handleChange}
+            >
+              <option value="Work">💼 Work</option>
+              <option value="Personal">🏠 Personal</option>
+              <option value="Ideas">💡 Ideas</option>
+            </select>
+          </div>
+          <div className="mb-4">
+            <label htmlFor="description" className="block font-semibold">
+              Description
+            </label>
+            <textarea
+              name="Description"
+              type="text"
+              value={formData.Description}
+              className="w-full rounded p-2 border"
+              onChange={handleChange}
+            />
+          </div>
+        </div>
+        <button className="w-full bg-purple-500 text-white py-2 rounded-lg cursor-pointer hover:bg-purple-600 transition-colors font-semibold">
+          Add Note
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Newforms;
