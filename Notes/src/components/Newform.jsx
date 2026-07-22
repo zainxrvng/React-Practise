1e2f5d5d288c9c15ab0940e0824ca76acb03ffd1
zainxrvng <muhammadzainkhan29@gmail.com>
2026-07-22 14:33:42 +0500

    Enhance Newforms component to toggle visibility of the note creation form and improve code formatting

--- Notes/src/components/Newform.jsx
+++ Notes/src/components/Newform.jsx
@@ -1,23 +1,24 @@
 import { useState } from "react";
-const Newforms = ( {notes, setNotes} ) => {
+const Newforms = ({ notes, setNotes }) => {
   const [formData, setFormData] = useState({
     Title: "",
     Priorty: "Medium",
     Category: "Work",
     Description: "",
   });
+  const [isFormVisable, setisFormVisable] = useState(false);
 
   const handleChange = (e) => {
-    setFormData({...formData, [e.target.name]: e.target.value})
-  }
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
 
   const handleSubmit = (e) => {
-    e.preventDefault()
-    if (!formData.Title || !formData.Description) return
-    
-    const newNotes = {id: Date.now(), ...formData}
+    e.preventDefault();
+    if (!formData.Title || !formData.Description) return;
 
-    setNotes([newNotes, ...notes])
+    const newNotes = { id: Date.now(), ...formData };
+
+    setNotes([newNotes, ...notes]);
 
     setFormData({
       Title: "",
@@ -25,73 +26,85 @@ const Newforms = ( {notes, setNotes} ) => {
       Category: "Work",
       Description: "",
     });
-      console.log("Form Submitted :", formData);
-
-  }
+    console.log("Form Submitted :", formData);
+  };
   return (
-    <div>
-      <form onSubmit= {handleSubmit}className="mb-6">
-        <div className="mb-4">
-          <label htmlFor="title" className="block font-semibold">
-            Title
-          </label>
-          <input
-            name="Title"
-            type="text"
-            value={formData.Title}
-            className="w-full rounded p-2 border"
-            onChange={handleChange}
-          />
-          <div className="mb-4">
-            <label htmlFor="priority" className="block font-semibold">
-              priority
-            </label>
-            <select
-              name="Priorty"
-              type="text"
-              value={formData.Priorty}
-              className="w-full rounded p-2 border"
-              onChange={handleChange}
-            >
-              <option value="High">🔴High</option>
-              <option value="Medium">🟠 Medium</option>
-              <option value="Low">🟢 Low</option>
-            </select>
-          </div>
-          <div className="mb-4">
-            <label htmlFor="Category" className="block font-semibold">
-              Category
-            </label>
-            <select
-              name="Category"
-              type="text"
-              value={formData.Category}
-              className="w-full rounded p-2 border"
-              onChange={handleChange}
-            >
-              <option value="Work">💼 Work</option>
-              <option value="Personal">🏠 Personal</option>
-              <option value="Ideas">💡 Ideas</option>
-            </select>
-          </div>
-          <div className="mb-4">
-            <label htmlFor="description" className="block font-semibold">
-              Description
-            </label>
-            <textarea
-              name="Description"
-              type="text"
-              value={formData.Description}
-              className="w-full rounded p-2 border"
-              onChange={handleChange}
-            />
-          </div>
-        </div>
-        <button className="w-full bg-purple-500 text-white py-2 rounded-lg cursor-pointer hover:bg-purple-600 transition-colors font-semibold">
-          Add Note
+    <>
+      <div>
+        <button
+          onClick={() => {
+            setisFormVisable(!isFormVisable);
+          }}
+          className="w-full bg-gray-100 border border-gray-300 text-purple-800 py-2 rounded-lg cursor-pointer hover: bg-purple-200 hover: border-purple-300 transition mb-4"
+        >
+          {" "}
+          {isFormVisable ? "Hide form ❌" : "add new note ➕"}
         </button>
-      </form>
-    </div>
+        {isFormVisable && (
+          <form onSubmit={handleSubmit} className="mb-6">
+            <div className="mb-4">
+              <label htmlFor="title" className="block font-semibold">
+                Title
+              </label>
+              <input
+                name="Title"
+                type="text"
+                value={formData.Title}
+                className="w-full rounded p-2 border"
+                onChange={handleChange}
+              />
+              <div className="mb-4">
+                <label htmlFor="priority" className="block font-semibold">
+                  priority
+                </label>
+                <select
+                  name="Priorty"
+                  type="text"
+                  value={formData.Priorty}
+                  className="w-full rounded p-2 border"
+                  onChange={handleChange}
+                >
+                  <option value="High">🔴High</option>
+                  <option value="Medium">🟠 Medium</option>
+                  <option value="Low">🟢 Low</option>
+                </select>
+              </div>
+              <div className="mb-4">
+                <label htmlFor="Category" className="block font-semibold">
+                  Category
+                </label>
+                <select
+                  name="Category"
+                  type="text"
+                  value={formData.Category}
+                  className="w-full rounded p-2 border"
+                  onChange={handleChange}
+                >
+                  <option value="Work">💼 Work</option>
+                  <option value="Personal">🏠 Personal</option>
+                  <option value="Ideas">💡 Ideas</option>
+                </select>
+              </div>
+              <div className="mb-4">
+                <label htmlFor="description" className="block font-semibold">
+                  Description
+                </label>
+                <textarea
+                  name="Description"
+                  type="text"
+                  value={formData.Description}
+                  className="w-full rounded p-2 border"
+                  onChange={handleChange}
+                />
+              </div>
+            </div>
+            <button className="w-full bg-purple-500 text-white py-2 rounded-lg cursor-pointer hover:bg-purple-600 transition-colors font-semibold">
+              Add Note
+            </button>
+          </form>
+        )}
+      </div>
+    </>
   );
 };
 
